cf540a51cd394ddf3762a63e15442c2be100b082
Javier Cardenas <neo1807@gmail.com>
2023-04-09 13:08:56 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Javier Cardenas on 04/09/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
